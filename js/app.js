fa8f9be9f3642d3c43a82c04670d937f09c24118
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-02-03 14:46:57 -1000

    display person 4 home world

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 console.log("sanity check");
 
+const oReq = new XMLHttpRequest();
+oReq.addEventListener("load", reqListener);
+oReq.open("GET", "https://swapi.co/api/people/4/");
+oReq.send();
+
 function reqListener() {
   console.log("reqListener callback fired");
   const data = JSON.parse(this.responseText);
@@ -7,9 +12,17 @@ function reqListener() {
 
   const person4Name = document.getElementById("person4Name");
   person4Name.innerHTML = "Name: " + data.name;
-}
 
-const oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "https://swapi.co/api/people/4/");
-oReq.send();
+  const oReq2 = new XMLHttpRequest();
+  oReq2.addEventListener("load", reqListener2);
+  oReq2.open("GET", "https://swapi.co/api/planets/1/");
+  oReq2.send();
+
+  function reqListener2() {
+    console.log("reqListener2 callback fired");
+    const data2 = JSON.parse(this.responseText);
+
+    const person4HomeWorld = document.getElementById("person4HomeWorld");
+    person4HomeWorld.innerHTML = "Home World: " + data.name;
+  }
+}
