ebb265502cc4c405b16486e0ff697ca8d2dce863
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-02-03 15:09:54 -1000

    display person 14 name and species

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 console.log("sanity check");
 
-const oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "https://swapi.co/api/people/4/");
-oReq.send();
+const oReqPerson4 = new XMLHttpRequest();
+oReqPerson4.addEventListener("load", reqListenerPerson4);
+oReqPerson4.open("GET", "https://swapi.co/api/people/4/");
+oReqPerson4.send();
 
-function reqListener() {
+function reqListenerPerson4() {
   console.log("reqListener callback fired");
   const data = JSON.parse(this.responseText);
   console.log(data);
@@ -23,6 +23,31 @@ function reqListener() {
     const data2 = JSON.parse(this.responseText);
 
     const person4HomeWorld = document.getElementById("person4HomeWorld");
-    person4HomeWorld.innerHTML = "Home World: " + data.name;
+    person4HomeWorld.innerHTML = "Home World: " + data2.name;
+  }
+}
+
+const oReqPerson14 = new XMLHttpRequest();
+oReqPerson14.addEventListener("load", reqListenerPerson14);
+oReqPerson14.open("GET", "https://swapi.co/api/people/14/");
+oReqPerson14.send();
+
+function reqListenerPerson14() {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+
+  const person14Name = document.getElementById("person14Name");
+  person14Name.innerHTML = "Name: " + data.name;
+
+  const oReq2 = new XMLHttpRequest();
+  oReq2.addEventListener("load", reqListener2);
+  oReq2.open("GET", "https://swapi.co/api/species/1/");
+  oReq2.send();
+
+  function reqListener2() {
+    const data2 = JSON.parse(this.responseText);
+
+    const person14Species = document.getElementById("person14Species");
+    person14Species.innerHTML = "Species: " + data2.name;
   }
 }
