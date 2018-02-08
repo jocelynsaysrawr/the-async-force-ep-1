9951436775b970e58faf50d0e6a8ba8645acbd21
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-02-07 20:21:08 -1000

    refactor functions

--- js/app.js
+++ js/app.js
@@ -1,100 +1,91 @@
 console.log("sanity check");
 
-const oReqPerson4 = new XMLHttpRequest();
-oReqPerson4.addEventListener("load", reqListenerPerson4);
-oReqPerson4.open("GET", "https://swapi.co/api/people/4/");
-oReqPerson4.send();
+const _request = function(url, callback) {
+  const oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", callback);
+  oReq.open("GET", url);
+  oReq.send();
+  return oReq;
+};
+
+const _createElement = function(element, className, innerHTML) {
+  let elementType = document.createElement(element);
+
+  if (className) {
+    elementType.className = className;
+  }
+  if (innerHTML) {
+    elementType.innerHTML = innerHTML;
+  }
+  return elementType;
+};
+
+const oReqPerson4 = _request(
+  "https://swapi.co/api/people/4/",
+  reqListenerPerson4
+);
 
 function reqListenerPerson4() {
-  console.log("reqListener callback fired");
   const data = JSON.parse(this.responseText);
   console.log(data);
+  document.getElementById("person4Name").innerHTML = "Name: " + data.name;
 
-  const person4Name = document.getElementById("person4Name");
-  person4Name.innerHTML = "Name: " + data.name;
-
-  const oReq2 = new XMLHttpRequest();
-  oReq2.addEventListener("load", reqListener2);
-  oReq2.open("GET", "https://swapi.co/api/planets/1/");
-  oReq2.send();
-
+  const person4Planet = _request(data.homeworld, reqListener2);
   function reqListener2() {
-    console.log("reqListener2 callback fired");
     const data2 = JSON.parse(this.responseText);
-
-    const person4HomeWorld = document.getElementById("person4HomeWorld");
-    person4HomeWorld.innerHTML = "Home World: " + data2.name;
+    document.getElementById("person4HomeWorld").innerHTML =
+      "Home World: " + data2.name;
   }
 }
 
-const oReqPerson14 = new XMLHttpRequest();
-oReqPerson14.addEventListener("load", reqListenerPerson14);
-oReqPerson14.open("GET", "https://swapi.co/api/people/14/");
-oReqPerson14.send();
+const oReqPerson14 = _request(
+  "https://swapi.co/api/people/14/",
+  reqListenerPerson14
+);
 
 function reqListenerPerson14() {
   const data = JSON.parse(this.responseText);
   console.log(data);
+  document.getElementById("person14Name").innerHTML = "Name: " + data.name;
 
-  const person14Name = document.getElementById("person14Name");
-  person14Name.innerHTML = "Name: " + data.name;
-
-  const oReq2 = new XMLHttpRequest();
-  oReq2.addEventListener("load", reqListener2);
-  oReq2.open("GET", "https://swapi.co/api/species/1/");
-  oReq2.send();
-
+  const person14Species = _request(data.species, reqListener2);
   function reqListener2() {
     const data2 = JSON.parse(this.responseText);
-
-    const person14Species = document.getElementById("person14Species");
-    person14Species.innerHTML = "Species: " + data2.name;
+    document.getElementById("person14Species").innerHTML =
+      "Species: " + data2.name;
   }
 }
 
-const oReqFilms = new XMLHttpRequest();
-oReqFilms.addEventListener("load", reqListenerFilms);
-oReqFilms.open("GET", "https://swapi.co/api/films/");
-oReqFilms.send();
+const oReqFilms = _request("https://swapi.co/api/films/", reqListenerFilms);
 
 function reqListenerFilms() {
   const data = JSON.parse(this.responseText);
   console.log(data);
   console.log(data.results);
 
-  const createfilmList = document.getElementById("filmList");
+  const createFilmList = document.getElementById("filmList");
   data.results.forEach(arr => {
-    const newFilm = document.createElement("li");
-    newFilm.className = "film";
-    createfilmList.appendChild(newFilm);
+    const newFilm = _createElement("li", "film");
 
-    const filmTitle = document.createElement("h2");
-    filmTitle.className = "filmTitle";
-    filmTitle.innerHTML = arr.title;
+    const filmTitle = _createElement("h2", "filmTitle", arr.title);
     newFilm.appendChild(filmTitle);
 
-    const Planets = document.createElement("h3");
-    Planets.innerHTML = "Planets";
+    const Planets = _createElement("h3", false, "Planets");
     newFilm.appendChild(Planets);
 
-    const filmPlanets = document.createElement("ul");
-    filmPlanets.className = "filmPlanets";
+    const filmPlanets = _createElement("ul", "filmPlanets");
     newFilm.appendChild(filmPlanets);
 
     arr.planets.forEach(arr => {
-      const planet = document.createElement("li");
-      planet.className = "planet";
-      const oReqPlanet = new XMLHttpRequest();
-      oReqPlanet.addEventListener("load", function(event) {
-        const data = JSON.parse(event.target.responseText);
-        const planetName = document.createElement("h4");
-        planetName.className = "planetName";
-        planetName.innerHTML = data.name;
+      const planet = _createElement("li", "planet");
+      const oReqPlanet = _request(arr, planetNameData);
+      function planetNameData() {
+        const data = JSON.parse(this.responseText);
+        const planetName = _createElement("h4", "planetName", data.name);
         planet.appendChild(planetName);
         filmPlanets.appendChild(planet);
-      });
-      oReqPlanet.open("GET", arr);
-      oReqPlanet.send();
+      }
     });
+    createFilmList.appendChild(newFilm);
   });
 }
